5d7a1cfd86a21b5132d34a232576ec8c6ddf5dc5
Peter Cashel <pacas00@gmail.com>
2018-10-25 03:53:47 +1000

    Initial Structure Commit.

---
+++ Source/plugin_petercashel.ModdersGearbox/UtilClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace plugin_petercashel_ModdersGearbox
+{
+	public class UtilClass
+	{
+	    public static string modName = "Modder's Gearbox";
+	    public const string modId = "petercashel.ModdersGearbox";
+
+        public static void WriteLine(string s)
+		{
+            Debug.LogWarning(UtilClass.modName + ": " + s);
+		}
+
+		public static void WriteLine(int x)
+		{
+            Debug.LogWarning(UtilClass.modName + ": " + x);
+		}
+
+		public static void WriteLine(float f)
+		{
+            Debug.LogWarning(UtilClass.modName + ": " + f);
+		}
+
+		public static void WriteLine(bool b)
+		{
+            Debug.LogWarning(UtilClass.modName + ": " + b);
+		}
+
+		internal static void WriteLine(Exception ex)
+		{
+            Debug.LogError(UtilClass.modName + ": " + ex.Message);
+            Debug.LogError(ex.StackTrace);
+		}
+
+	}
+}
---
+++ Source/plugin_petercashel.ModdersGearbox/plugin_petercashel_ModdersGearbox.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Harmony;
+using UnityEngine;
+
+namespace plugin_petercashel_ModdersGearbox
+{
+    public class plugin_petercashel_ModdersGearbox : FortressCraftMod
+	{
+		private bool _Patched;
+		private HarmonyInstance _HarmonyInstance;
+		private bool mInitalised;
+		private bool mIsRunning;
+		private string mWorkingDir;
+
+		public plugin_petercashel_ModdersGearbox()
+		{
+			Harmony();
+		}
+
+		public void Harmony()
+		{
+
+			if (_Patched) return;
+			_Patched = true;
+			_HarmonyInstance = HarmonyInstance.Create(UtilClass.modId);
+			_HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+
+			foreach (Assembly assemb in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				_HarmonyInstance.PatchAll(assemb);
+			}
+
+			try
+			{
+				Debug.Log(UtilClass.modName + " - Harmony Patching Complete");
+			}
+			catch
+			{
+			}
+		}
+
+		public void Awake()
+		{
+			Harmony();
+		}
+
+		public void Start()
+		{
+			foreach (ModConfiguration current in ModManager.mModConfigurations.Mods)
+			{
+				if (current.Id.Contains(UtilClass.modId))
+				{
+					UtilClass.WriteLine($"Found {current.Id} {current.Name}");
+					UtilClass.modName = current.Name;
+					mWorkingDir = current.Path;
+					break;
+				}
+			}
+
+			UtilClass.WriteLine("Loaded! Using data directory " + mWorkingDir);
+			mInitalised = true;
+			mIsRunning = true;
+		}
+
+
+	}
+}
